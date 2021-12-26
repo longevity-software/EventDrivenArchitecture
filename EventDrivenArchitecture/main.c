d1153841ad4cdbd643dee3d84a97d3b0cf7b6e2f
Graham Long <38405059+longevity-software@users.noreply.github.com>
2021-12-26 19:28:07 +0000

    Initial commit of blank Atmel Studio project.

---
+++ EventDrivenArchitecture/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 12/26/2021 7:24:45 PM
+ *  Author: Graham
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}

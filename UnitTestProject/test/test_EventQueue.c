2c53f10d19e359d03420c9b98a0e9bd57e1f44b1
Graham Long <38405059+longevity-software@users.noreply.github.com>
2021-12-26 20:06:36 +0000

    Added initial unit testing module for the EventQueue.

---
+++ UnitTestProject/test/test_EventQueue.c
@@ -0,0 +1,20 @@
+#ifdef TEST
+
+#include "unity.h"
+
+#include "EventQueue.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_EventQueue_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement EventQueue");
+}
+
+#endif // TEST
